55e4ac4a58513d53ed08130f58a652a34bf6438c
noxipicous <noxipicous@gmail.com>
2017-01-28 00:59:30 -0500

    Setting up for basic answer display

--- src/index.js
+++ src/index.js
@@ -13,28 +13,38 @@ const app = () => {
 
 const stackOverflowDao = function($http) {
   this.$inject = ['$http'];
-  const baseUrl = 'https://api.stackexchange.com/2.2/questions/featured?page=';
+  const baseUrl = 'https://api.stackexchange.com/2.2/questions';
 
   return { 
-    getQuestions: getQuestions 
+    getQuestions: getQuestions,
+    getAnswersForQuestion: getAnswersForQuestion
   };
 
 
   function getQuestions(pageNumber=1, pagesize=10, sortOrder='desc'){
+    const queryUrl = `${baseUrl}/featured?page=${pageNumber}&pagesize=${pagesize}&order=${sortOrder}&sort=activity&site=stackoverflow`;
     const request = {
-      method: 'GET',
-      url: `${baseUrl}${pageNumber}&pagesize=${pagesize}&order=${sortOrder}&sort=activity&site=stackoverflow`,
-      headers: { 'Content-Type': 'application/json' }
+      method: 'GET', url: queryUrl, headers: { 'Content-Type': 'application/json' }
+    }
+
+    return $http(request);
+  }
+  function getAnswersForQuestion(questionId){
+    const queryUrl = `${baseUrl}/${questionId}/answers?order=desc&sort=activity&site=stackoverflow`;
+    const request = {
+      method: 'GET', url: queryUrl, headers: { 'Content-Type': 'application/json' }
     }
 
     return $http(request);
   }
 };
+
 const stackOverflowDataService = function(stackOverflowDao) {
   this.$inject = ['stackOverflowDao'];
 
   return {
-    getQuestionsWithAnswers: getQuestionsWithAnswers
+    getQuestionsWithAnswers: getQuestionsWithAnswers,
+    getAnswersForQuestion: getAnswersForQuestion
   };
 
   // This function is intentionally private
@@ -54,6 +64,17 @@ const stackOverflowDataService = function(stackOverflowDao) {
       return questionsWithAnswers;
     }
   }
+  function getAnswersForQuestion(questionId){
+    return stackOverflowDao.getAnswersForQuestion(questionId);
+  }
+};
+
+const unsafeFilter = function ($sce) {
+   return function (val) {
+      if( (typeof val == 'string' || val instanceof String) ) {
+         return $sce.trustAsHtml(val);
+      }
+   }; 
 };
 
 
@@ -61,13 +82,32 @@ const AppCtrl = function(stackOverflowDataService){
   this.$inject = ['stackOverflowDataService'];
   const vm = this;
 
-  stackOverflowDataService
-    .getQuestionsWithAnswers()
-    .then(displayResults);
+  vm.initialize = initialize;
+  vm.getAnswersForQuestion = getAnswersForQuestion;
 
-  function displayResults(results){
-    console.log('questions query results: ', results);
-    vm.questions = results;
+  initialize();
+
+
+  function initialize(){
+    stackOverflowDataService
+      .getQuestionsWithAnswers()
+      .then(displayResults);
+
+    function displayResults(results){
+      console.log('questions query results: ', results);
+      vm.questions = results;
+    }
+  }
+
+  function getAnswersForQuestion(questionId=0){
+      stackOverflowDataService
+        .getAnswersForQuestion(questionId)
+        .then(displayResults);
+
+    function displayResults(results){
+      console.log('answers for question query results: ', results);
+      vm.answers = results;
+    }  
   }
 };
 
@@ -77,11 +117,5 @@ angular.module(MODULE_NAME, [])
   .controller('AppCtrl', AppCtrl)
   .factory('stackOverflowDataService', stackOverflowDataService)
   .factory('stackOverflowDao', stackOverflowDao)
-  .filter('unsafe', function ($sce) {
-   return function (val) {
-      if( (typeof val == 'string' || val instanceof String) ) {
-         return $sce.trustAsHtml(val);
-      }
-   };
-});
+  .filter('unsafe', unsafeFilter);
  
